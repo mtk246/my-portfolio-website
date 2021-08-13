e4a61a9aff62985e9629e013a17e5db6e6d8fd3a
mtk246 <minthukyaw454@gmail.com>
2021-08-13 14:00:53 +0630

    SkillBar section fix >>> changed from using flex system to grid system

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -7,41 +7,41 @@ function SkillComponent(props) {
 
   function SkillBarDiv() {
     return (
-      <div className="flex flex-col w-4/6 flex-1">
-        <div className="relative w-full rounded m-auto flex flex-row items-center px-8">
-          <div
-            className="absolute top-0 h-4 rounded shim-red"
-            style={style1}
-          ></div>
-        </div>
-        <div className="relative w-full rounded m-auto flex flex-row items-center px-8">
-          <div
-            className="absolute top-0 h-4 rounded shim-blue"
-            style={style2}
-          ></div>
-        </div>
-        <div class="relative w-full rounded m-auto flex flex-row items-center px-8">
-          <div
-            className="absolute top-0 h-4 rounded shim-green"
-            style={style3}
-          ></div>
+      <div className="flex flex-col w-full flex-1 my-4 lg:my-0">
+        <div className="grid grid-cols-1 grid-rows-3 gap-4">
+          <div className="relative w-full rounded m-auto flex flex-col md:flex-row items-start md:items-center px-0 md:px-8">
+            <h1 className="text-xl mx-4">{props.skillName1}</h1>{" "}
+            <div
+              className="absolute top-0 h-4 rounded shim-red"
+              style={style1}
+            ></div>
+          </div>
+          <div className="relative w-full rounded m-auto flex flex-col md:flex-row items-start md:items-center px-0 md:px-8">
+            <h1 className="text-xl mx-4">{props.skillName2}</h1>
+            <div
+              className="absolute top-0 h-4 rounded shim-blue"
+              style={style2}
+            ></div>
+          </div>
+          <div class="relative w-full rounded m-auto flex flex-col md:flex-row items-start md:items-center px-0 md:px-8">
+            <h1 className="text-xl mx-4">{props.skillName3}</h1>
+            <div
+              className="absolute top-0 h-4 rounded shim-green"
+              style={style3}
+            ></div>
+          </div>
         </div>
       </div>
     );
   }
   return (
-    <BodyContent className="body-content flex flex-row shadow my-3">
-      <div className="px-12 bg-gray-200 rounded-lg p-4 m-auto flex flex-none w-1/3">
-        <h1 className="text-black text-xl font-extrabold">
+    <BodyContent className="body-content flex flex-col lg:flex-row shadow my-3">
+      <div className="bg-gray-200 rounded-lg p-4 m-auto flex flex-none w-full md:w-1/2 lg:w-1/3">
+        <h1 className="text-black text-lg md:text-xl font-extrabold">
           {props.skillTitle}
         </h1>
       </div>
-      <div className="ml-3">
-        <h1 className="text-xl my-1">{props.skillName1}</h1>
-        <h1 className="text-xl my-1">{props.skillName2}</h1>
-        <h1 className="text-xl my-1">{props.skillName3}</h1>
-      </div>
-      <SkillBarDiv />
+      <SkillBarDiv className="ml-3" />
     </BodyContent>
   );
 }
@@ -62,18 +62,22 @@ export default function Skills() {
       </BodyContent>
       <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 w-3/4 lg:w-full">
         <SkillComponent
-          skillTitle="development"
+          skillTitle="develop"
           skillName1="web"
-          progress1={75}
-          progress2={50}
-          progress3={66}
+          skillName2="flutter"
+          skillName3="react"
+          progress1={100}
+          progress2={40}
+          progress3={75}
         />
         <SkillComponent
-          skillTitle="development"
-          skillName1="web"
-          progress1={75}
-          progress2={50}
-          progress3={66}
+          skillTitle="design and editing"
+          skillName1="photoshop"
+          skillName2="illustrator"
+          skillName3="aftereffect"
+          progress1={60}
+          progress2={80}
+          progress3={35}
         />
       </div>
     </div>
