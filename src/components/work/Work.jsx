62e5ac0e9807d218a1cb970d85bca820e815d132
mtk246 <minthukyaw454@gmail.com>
2021-08-17 22:14:27 +0630

    Work Section Day-2

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,8 +1,6 @@
-import { BodyContent } from "../const";
-
 export default function Work() {
   return (
-    <div className="grid grid-col grid-cols-1 lg:grid-cols-3 gap-4">
+    <div className="grid grid-col grid-cols-1 lg:grid-cols-2 gap-4">
       <div className="col-span-1 m-auto p-5 bg-gray-400 bg-opacity-30 rounded-2xl shadow-xl transform-gpu skew-y-4 rotate-6">
         <div className="transform-gpu -skew-y-4 -rotate-6">
           <h1 className="text-red-500 font-bold text-9xl tracking-widest">
@@ -16,33 +14,48 @@ export default function Work() {
           </p>
         </div>
       </div>
-      <BodyContent className="body-content col-span-2 text-center p-4">
-        <h1 className="text-red-500 font-bold text-4xl p-4">Creativities</h1>
-        <div className="grid grid-cols-2 gap-4">
-          <div className="col-span-1 bg-red-300 bg-opacity-30 rounded-2xl transform-gpu rotate-12 h-3/4 mx-9">
-            <div className="transform-gpu -rotate-12">
-              <h1 className="py-3"> Valentine Day App with Flutter</h1>
-              <img src="https://picsum.photos/300" alt="" className="mx-auto" />
-            </div>
-          </div>
-          <div className="col-span-1 bg-red-300 bg-opacity-30 rounded-2xl transform-gpu rotate-12 h-3/4 mx-9">
-            <div className="transform-gpu -rotate-12">
-              <h1 className="py-3"> Shop App with Flutter</h1>
-              <img src="https://picsum.photos/300" alt="" className="mx-auto" />
-            </div>
-          </div>
-          <div className="col-span-1 bg-red-300 bg-opacity-30 rounded-2xl transform-gpu rotate-12 h-3/4 mx-9">
-            <div className="transform-gpu -rotate-12">
-              <h1 className="py-3"> Furniture Website with PHP</h1>
-              <img src="https://picsum.photos/300" alt="" className="mx-auto" />
-            </div>
-            <div className="transform-gpu -rotate-12">
-              <h1 className="py-3"> Furniture Website with PHP</h1>
-              <img src="https://picsum.photos/300" alt="" className="mx-auto" />
-            </div>
-          </div>
+      <div class="grid grid-flow-col grid-rows-2 grid-cols-3 gap-4 bg-red-200 text-center">
+        <div class="transform scale-110 -rotate-6 bg-gray-200 rounded-xl p-3">
+          Valentine Day App with Flutter
+          <img
+            src="https://picsum.photos/200"
+            alt=""
+            className="m-auto p-3 rounded-3xl"
+          />
         </div>
-      </BodyContent>
+        <div class="col-start-3 transform scale-75 rotate-6 translate-x-2 translate-y-15 bg-gray-200 rounded-xl p-3">
+          React Native
+          <img
+            src="https://picsum.photos/200"
+            alt=""
+            className="m-auto p-3 rounded-3xl"
+          />
+        </div>
+        <div class="transform scale-150 translate-y-11 bg-gray-200 rounded-xl p-3">
+          Library App with TKinter Python
+          <img
+            src="https://picsum.photos/200"
+            alt=""
+            className="m-auto p-3 rounded-3xl"
+          />
+        </div>
+        <div class="transform translate-y-12 translate-x-10 -rotate-12 bg-gray-200 rounded-xl p-3">
+          Valentine Day Website
+          <img
+            src="https://picsum.photos/200"
+            alt=""
+            className="m-auto p-3 rounded-3xl"
+          />
+        </div>
+        <div class="row-start-1 col-start-2 col-span-2 transform translate-x-20 translate-y-4 scale-90 bg-gray-200 rounded-xl p-3">
+          Furniture Website with PHP
+          <img
+            src="https://picsum.photos/200"
+            alt=""
+            className="m-auto p-3 rounded-3xl"
+          />
+        </div>
+      </div>
     </div>
   );
 }
